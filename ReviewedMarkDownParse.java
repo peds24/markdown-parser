d31db1e4e7e301cd423ca3720f8974e98a9c32e3
Pedro Serdio <serdiopedro@gmail.com>
2022-05-18 16:43:31 -0700

    test files created

--- ReviewedMarkDownParse.java
+++ ReviewedMarkDownParse.java
@@ -3,7 +3,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-public class ReviewedMarkDownParse {
+public class ReviewedMarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
